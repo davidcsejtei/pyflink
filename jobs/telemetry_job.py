2d1630bf0d0da41b3ef0dc5a38cec335160cb6c4
csejtei1dav713 <csejteidavid90@gmail.com>
2025-07-17 11:01:53 +0200

    up and running first job

--- jobs/telemetry_job.py
+++ jobs/telemetry_job.py
@@ -4,6 +4,9 @@ from pyflink.common.typeinfo import Types
 env = StreamExecutionEnvironment.get_execution_environment()
 env.set_parallelism(1)
 
+# Configure for cluster execution
+env.get_config().set_auto_watermark_interval(1000)
+
 data = env.from_collection(
     collection=[("Starship", 1200), ("Falcon9", 910)],
     type_info=Types.TUPLE([Types.STRING(), Types.INT()])
